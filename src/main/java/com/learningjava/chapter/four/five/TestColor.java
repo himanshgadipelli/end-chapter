d6087143e8f7096bb1b150ef0894b4fab7f049a5
bobby <himanshgadipelli@gmail.com>
2017-01-23 11:43:31 -0500

    Till problem 5

---
+++ src/main/java/com/learningjava/chapter/four/five/TestColor.java
@@ -0,0 +1,28 @@
+package com.learningjava.chapter.four.five;
+
+/**
+ * Created by bobby on 1/23/2017.
+ */
+abstract class Shape{
+    public static class Color{
+        int m_red, m_green, m_blue;
+        public Color(){
+            this(0,0,0);
+        }
+        public Color(int red, int green, int blue){
+            m_red = red;
+            m_green = green;
+            m_blue = blue;
+        }
+        public String toString(){
+            return "Red = "+m_red+" Green = "+m_green+" Blue = "+m_blue;
+        }
+    }
+}
+public class TestColor {
+    public static void main(String [] args){
+        Shape.Color white = new Shape.Color(255,255,255);
+        System.out.println("White color has values " + white);
+
+    }
+}
---
+++ src/main/java/com/learningjava/chapter/four/three/Counter.java
@@ -0,0 +1,20 @@
+package com.learningjava.chapter.four.three;
+
+/**
+ * Created by bobby on 1/23/2017.
+ */
+public class Counter {
+    private static int count;
+    public Counter(){
+        count++;
+    }
+    public static void printCount(){
+        System.out.println("Number of instances created so far = "+count);
+    }
+    public static void main(String []args){
+        Counter anInstance = new Counter();
+        Counter.printCount();
+        Counter anotherInstance = new Counter();
+        Counter.printCount();
+    }
+}
