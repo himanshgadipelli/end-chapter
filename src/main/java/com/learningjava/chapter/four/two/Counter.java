d16947ab5a1aacf1d28d99692a5713bbecb77b58
bobby <himanshgadipelli@gmail.com>
2017-01-23 11:07:39 -0500

    Start of chapter 4

---
+++ src/main/java/com/learningjava/chapter/four/two/Counter.java
@@ -0,0 +1,20 @@
+package com.learningjava.chapter.four.two;
+
+/**
+ * Created by bobby on 1/23/2017.
+ */
+public class Counter {
+    private int count;
+    public Counter(){
+        count++;
+    }
+    public void printCount(){
+        System.out.println("Number of instances created so far is: "+count);
+    }
+    public static void main(String []args){
+        Counter anInstance = new Counter();
+        anInstance.printCount();
+        Counter anotherInstance = new Counter();
+        anotherInstance.printCount();
+    }
+}
